0b601c08f18c24a5fac4b97eecb445ee94078111
Anisio Mandlate <anisiomandlate@gmail.com>
2020-08-04 18:28:01 +0200

    feat: Add CORS instance and method to list all connections

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import ClassesControlers from "./controllers/ClassControllers";
+import ConnectionsController from "./controllers/ConnectionsControllers";
 
 const routes = express.Router();
 const classesControllers = new ClassesControlers();
+const connectionsControllers = new ConnectionsController();
 
 routes.get("/classes", classesControllers.index);
 routes.post("/classes", classesControllers.create);
+
+routes.get("/connections", connectionsControllers.index);
+routes.post("/connections", connectionsControllers.create);
 
 export default routes;
